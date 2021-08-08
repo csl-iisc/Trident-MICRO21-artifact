d1634d075bf692641d7d2580f22cfa881da9e249
Ashish Panwar <31126399+apanwariisc@users.noreply.github.com>
2021-08-08 19:13:04 +0530

    Update prep_all_datasets.sh

--- scripts/prep_all_datasets.sh
+++ scripts/prep_all_datasets.sh
@@ -4,13 +4,13 @@ SCRIPTS=$(dirname `readlink -f "$0"`)
 ROOT="$(dirname "$SCRIPTS")"
 
 echo "Generating canneal and svm datasets..."
-bash $SCRIPTS/prep_canneal_dataset.sh > /dev/null 2>&1 &
+bash $ROOT/datasets/prep_canneal_dataset.sh > /dev/null 2>&1 &
 PID1=$!
-bash $SCRIPTS/prep_svm_dataset.sh >/dev/null 2>&1 &
+bash $ROOT/datasets/prep_svm_dataset.sh >/dev/null 2>&1 &
 PID2=$!
 wait $PID1
 echo "Generating fragmentation datasets..."
-$SCRIPTS/prep_fragmentation_dataset.py
+$ROOT/datasets/prep_fragmentation_dataset.py
 wait $PID2
 sync
 echo "Done..."
